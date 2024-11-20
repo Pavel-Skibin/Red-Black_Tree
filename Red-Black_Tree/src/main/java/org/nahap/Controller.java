f885a99b455c6bc44decdd55507edb0e03f71d70
Pavel <SkibinPS05@gmail.com>
2024-11-20 18:45:22 +0300

    added image customization

--- Red-Black_Tree/src/main/java/org/nahap/Controller.java
+++ Red-Black_Tree/src/main/java/org/nahap/Controller.java
@@ -2,11 +2,14 @@ package org.nahap;
 
 import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.engine.Format;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import org.nahap.RBTreeGraphvizConverter;
 
 import javax.imageio.ImageIO;
@@ -17,41 +20,72 @@ import java.io.ByteArrayOutputStream;
 public class Controller {
 
     @FXML
-    private Canvas canvas;  // Ссылка на Canvas из FXML
+    private Canvas canvas;
+
+    @FXML
+    private ScrollPane scrollPane;
 
     @FXML
     public void initialize() {
-        // Получаем GraphicsContext для рисования
+        AnchorPane parentPane = (AnchorPane) canvas.getParent();
+
+        // Динамическая настройка размера Canvas относительно ScrollPane
+        parentPane.widthProperty().addListener((obs, oldVal, newVal) -> {
+            double scrollWidth = scrollPane.getWidth();
+            double newCanvasWidth = newVal.doubleValue() - scrollWidth - 30; // Учитываем отступы
+            canvas.setWidth(newCanvasWidth);
+        });
+
+        parentPane.heightProperty().addListener((obs, oldVal, newVal) -> {
+            canvas.setHeight(newVal.doubleValue() - 20); // Учитываем отступы
+        });
+
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        // Создание дерева
         RBTree<Integer> tree = new RBTree<>();
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 3; i <= 500; i+=1) {
             tree.put(i);
         }
 
-        // Генерация DOT-строки для дерева
-        String dot = RBTreeGraphvizConverter.convertToDot(tree); // Предполагаем, что этот метод существует
-        System.out.println(dot);
+        String dot = RBTreeGraphvizConverter.convertToDot(tree);
 
-        try {
-            // Генерация BufferedImage
-            BufferedImage bufferedImage = Graphviz.fromString(dot)
-                    .render(Format.PNG)
-                    .toImage();
+        Platform.runLater(() -> {
+            try {
+                BufferedImage bufferedImage = Graphviz.fromString(dot)
+                        .render(Format.PNG)
+                        .toImage();
 
-            // Конвертация BufferedImage в byte[]
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(bufferedImage, "png", baos);
-            byte[] imageData = baos.toByteArray();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ImageIO.write(bufferedImage, "png", baos);
+                byte[] imageData = baos.toByteArray();
 
-            // Создание объекта JavaFX Image из массива байтов
-            Image image = new Image(new ByteArrayInputStream(imageData));
+                Image image = new Image(new ByteArrayInputStream(imageData));
 
-            // Рисуем изображение на Canvas
-            gc.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+                // Масштабирование изображения по размеру Canvas с сохранением пропорций
+                double canvasWidth = canvas.getWidth();
+                double canvasHeight = canvas.getHeight();
+                double imageRatio = image.getWidth() / image.getHeight();
+                double canvasRatio = canvasWidth / canvasHeight;
+
+                double drawWidth, drawHeight, x, y;
+                if (imageRatio > canvasRatio) {
+                    // Изображение шире, чем Canvas
+                    drawWidth = canvasWidth;
+                    drawHeight = canvasWidth / imageRatio;
+                    x = 0;
+                    y = (canvasHeight - drawHeight) / 2;
+                } else {
+                    // Изображение выше, чем Canvas
+                    drawHeight = canvasHeight;
+                    drawWidth = canvasHeight * imageRatio;
+                    x = (canvasWidth - drawWidth) / 2;
+                    y = 0;
+                }
+
+                gc.drawImage(image, x, y, drawWidth, drawHeight);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
